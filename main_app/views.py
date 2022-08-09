dc75217c90c9ce1308ba214631dbde9fc07ce324
recipra <jmo0425@outlook.com>
2022-08-08 20:54:29 -0400

    add equipment index functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Equipment
 
 # Create your views here.
 
@@ -7,3 +8,9 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def equipment_index(request):
+  tools = Equipment.objects.filter(type='T')
+  shelters = Equipment.objects.filter(type='S')
+  outfits = Equipment.objects.filter(type='O')
+  return render(request, 'equipment/index.html', { 'tools': tools, 'shelters': shelters, 'outfits': outfits })
